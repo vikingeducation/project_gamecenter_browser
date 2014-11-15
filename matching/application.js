4d61db407410ae284863f23fa0759af7848471bc
tim5046 <tim5046@gmail.com>
2014-11-15 12:29:59 -0800

    random card generation

--- matching/application.js
+++ matching/application.js
@@ -5,11 +5,36 @@ var matchingGame = {
 			$(".cards").append('<tr></tr>');
 
 			for(var j=0; j<num; j++) {
-				$(".cards tr").append('<td>X</td>');
+				$(".cards tr").last().append('<td></td>');
 			}
-		}
-	}
+		}	
+		this.add_value_to_cell(num);
+	},
 
+	generate_matches: function(num){
+		var num_pairs = (num*num)/2;
+		var pairs = [];
+		for (var i=0; i<num_pairs; i++) {
+			random_num = i ;
+			pairs.push(random_num);
+			pairs.push(random_num);
+		};
+		
+		return pairs;
+	},
+
+	add_value_to_cell: function(num){
+		var pairs_array = matchingGame.generate_matches(num);
+		$('td').each(function(){
+			$(this).text(pairs_array.shuffle().pop());	
+		});
+	}
 };
 
-$( document ).ready( function(){matchingGame.init(2);} );
+Array.prototype.shuffle = function(){
+ for(var j, x, i = this.length; i; j = Math.floor(Math.random() * i), x = this[--i], this[i] = this[j], this[j] = x);
+    return this;
+}
+
+
+$( document ).ready( function(){matchingGame.init(4);} );
