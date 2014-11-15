082d2db45c88e8e05e6309efeac8bada77f1b53f
Andur Carr <carr.andur@gmail.com>
2014-11-15 13:23:08 -0800

    Finish core game mechanics

--- matching/application.js
+++ matching/application.js
@@ -5,9 +5,9 @@ var matchingGame = {
 			$(".cards").append('<tr></tr>');
 
 			for(var j=0; j<num; j++) {
-				$(".cards tr").last().append('<td></td>');
+				$(".cards tr").last().append('<td class="hidden-card"></td>');
 			}
-		}	
+		}
 		this.add_value_to_cell(num);
 	},
 
@@ -18,15 +18,15 @@ var matchingGame = {
 			random_num = i ;
 			pairs.push(random_num);
 			pairs.push(random_num);
-		};
-		
+		}
+
 		return pairs;
 	},
 
 	add_value_to_cell: function(num){
 		var pairs_array = matchingGame.generate_matches(num);
 		$('td').each(function(){
-			$(this).text(pairs_array.shuffle().pop());	
+			$(this).text(pairs_array.shuffle().pop());
 		});
 		matchingGame.play_game();
 	},
@@ -37,12 +37,16 @@ var matchingGame = {
 		$('td').on('click',function(){
 			if (choices.length ==2){
 				choices=[];
-			};
-
-			choices.push($(this).text());
-			if (choices[0] == choices[1]){
-				alert('Match!');
-			};	
+			}
+      $(this).removeClass("hidden-card");
+			choices.push($(this));
+			if (choices[0].text() == choices[1].text()){
+        alert('Match!');
+      } else if (choices[1]) {
+        setTimeout(function() {
+        choices[0].addClass("hidden-card");
+        choices[1].addClass("hidden-card"); }, 1000);
+      }
 		});
 	},
 
@@ -56,7 +60,7 @@ var matchingGame = {
 Array.prototype.shuffle = function(){
  for(var j, x, i = this.length; i; j = Math.floor(Math.random() * i), x = this[--i], this[i] = this[j], this[j] = x);
     return this;
-}
+};
 
 
 $( document ).ready( function(){matchingGame.init(4);} );
