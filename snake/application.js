974e33e058894eb8f6bd21b9cc3f6d5568c1e768
Andur Carr <carr.andur@gmail.com>
2014-11-15 22:48:30 -0800

    Add score to snake game display

--- snake/application.js
+++ snake/application.js
@@ -1,115 +1,106 @@
-	LEFT=37;
-	RIGHT=39;
-	UP=38;
-	DOWN=40;
+  LEFT=37;
+  RIGHT=39;
+  UP=38;
+  DOWN=40;
 var snakeGame = {
-
-
-	init: function(){
-		var num = 20;
-		var counter=1;
-    snakeLength = [];
+  init: function(){
+    var num = 20;
+    var counter=1;
     for (var i=0; i<num; i++) {
-			$(".gameBoard").append('<tr></tr>');
-			for(var j=0; j<num; j++) {
-				$(".gameBoard tr").last().append('<td id='+counter+'></td>');
-				counter+=1;
-			}
-		}
-		snakeGame.draw_snake(num);
-		snakeGame.draw_food(num);
-		snakeGame.play(num);
+      $(".gameBoard").append('<tr></tr>');
+      for(var j=0; j<num; j++) {
+        $(".gameBoard tr").last().append('<td id='+counter+'></td>');
+        counter+=1;
+      }
+    }
+    snakeGame.draw_snake(num);
+    snakeGame.draw_food(num);
+    snakeGame.play(num);
   },
 
+  snake: [],
+
   draw_snake: function(num){
-  	snake_id = Math.round(Math.random()*(num*num)) + 1;
-  	$(".gameBoard td#"+snake_id).addClass("snake-head");
-    snakeLength.push(parseInt(snake_id));
+    snake_id = Math.round(Math.random()*(num*num)) + 1;
+    $(".gameBoard td#"+snake_id).addClass("snake-head snake");
+    snakeGame.snake.push(snake_id);
   },
 
   draw_food: function(num){
-  	var food_id = Math.round(Math.random()*(num*num)) + 1;
-  	while (food_id == snake_id){
-  		food_id = Math.round(Math.random()*(num*num)) + 1;
-  	}
-  	$(".gameBoard td#"+food_id).addClass("food");
+    var food_id = Math.round(Math.random()*(num*num)) + 1;
+    while ($(".gameBoard td#"+food_id).hasClass('snake')){
+      food_id = Math.round(Math.random()*(num*num)) + 1;
+    }
+    $(".gameBoard td#"+food_id).addClass("food");
   },
 
   play: function(num){
-  	var currentMove = '';
+    var currentMove = '';
 
-		setInterval(function() {
-		  if (currentMove !== '') {
-		    snakeGame.move(currentMove,num);
-		  }
-		}, 100);
+    setInterval(function() {
+      if (currentMove !== '') {
+        snakeGame.move(currentMove,num);
+      }
+    }, 100);
 
-		$(document).keydown(function(event) {
-		  if (event.which === RIGHT) {
-		    currentMove = 'r';
-		  } else if (event.which === LEFT) {
-		    currentMove = 'l';
-		  } else if (event.which === UP) {
-		    currentMove = 'u';
-		  } else if (event.which === DOWN) {
-		    currentMove = 'd';
-		  }
-		});
+    $(document).keydown(function(event) {
+      if (event.which === RIGHT) {
+        currentMove = 'r';
+      } else if (event.which === LEFT) {
+        currentMove = 'l';
+      } else if (event.which === UP) {
+        currentMove = 'u';
+      } else if (event.which === DOWN) {
+        currentMove = 'd';
+      }
+    });
 
   },
 
   move: function(direction,num){
-  	var currentID = parseInt($('td.snake-head').attr('id'));
+    var currentID = parseInt($('td.snake-head').attr('id'));
     var nextID;
 
-  	switch(direction) {
-			case 'r':
-				nextID = currentID+1;
-				break;
-			case 'l':
-				nextID = currentID-1;
-				break;
-			case 'u':
-				nextID = currentID-num;
-				break;
-			case 'd':
-				nextID = currentID+num;
-				break;
-		}
+    switch(direction) {
+      case 'r':
+        nextID = currentID+1;
+        break;
+      case 'l':
+        nextID = currentID-1;
+        break;
+      case 'u':
+        nextID = currentID-num;
+        break;
+      case 'd':
+        nextID = currentID+num;
+        break;
+    }
 
-		$('#'+currentID).removeClass('snake-head');
-		$('#'+nextID).addClass('snake-head');
+    $('#'+currentID).removeClass('snake-head');
+    $('#'+nextID).addClass('snake-head');
 
     if($('#'+nextID).hasClass('food'))
       { $('#'+nextID).removeClass('food');
-        snakeGame.draw_food(num); }
+        snakeGame.draw_food(num);
+        $('#score-box').text(snakeGame.snake.length+1); }
     else
-      { snakeLength.shift(); }
+      { snakeGame.snake.shift(); }
 
-    if($('#'+nextID).hasClass('snake')
-      || nextID > num*num || nextID < 1
-      || (nextID % num == 0 && nextID == currentID - 1)
-      || (currentID % num == 0 && nextID == currentID + 1) )
-      { alert('GAME OVER\nFinal Length:' + (snakeLength.length + 1));
-        snakeLength = [];
+    if($('#'+nextID).hasClass('snake') || nextID > num*num || nextID < 1 || (nextID % num === 0 && nextID == currentID - 1) || (currentID % num === 0 && nextID == currentID + 1) )
+      { alert('GAME OVER\nFinal Length: ' + (snakeGame.snake.length + 1));
+        snakeGame.snake = [];
         currentMove = undefined;
         $('td').removeClass('snake');
         $('td').removeClass('snake-head'); }
     else {
-      snakeLength.push(nextID);
+      snakeGame.snake.push(nextID);
 
       $('td').removeClass('snake');
 
-      snakeLength.forEach(function(section)
+      snakeGame.snake.forEach(function(section)
         { $("#"+section).addClass('snake'); });
     }
-
   }
-
-}
-
-
-
-
+};
 
 $( document ).ready( function(){snakeGame.init();} );
