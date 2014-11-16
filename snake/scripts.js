ce50fb580808242647934b867e7a33ce94632d5b
pndpo <peterdepaulo@gmail.com>
2014-11-16 14:56:20 -0800

    render snake

--- snake/scripts.js
+++ snake/scripts.js
@@ -18,21 +18,29 @@ var snake = {
   //a queue we can push onto and shift off of
   "snakeCoords": [],
 
+  snakeHead : function () {
+    return this.snakeCoords[this.snakeCoords.length - 1];
+  },
 
+  // this is for the snake array with coordinates
   createSnake : function(){
     //pick spot for snake to appear
     //push those coords onto snakeCoords
-    this.snakeCoords.push([0,0]);
-    this.snakeCoords.push([0,1]);
+    length = 3
+    for ( i = 0; i < length; i++) {
+      this.snakeCoords.push([0, i]);
+      this.findCoords(this.snakeHead()).addClass("snake");
+    }
+
+      this.lastTail = []
   },
 
-  //this looks at the direction, then pushes on 
+  //this looks at the direction, then pushes on
   //the next block in that direction,
   //then shifts off the last block
   moveSnake : function(){
-    var snakeHead = this.snakeCoords[this.snakeCoords.length - 1];
-    var headX = snakeHead[0];
-    var headY = snakeHead[1];
+    var headX = this.snakeHead()[0];
+    var headY = this.snakeHead()[1];
 
     console.log(this.direction);
     switch(this.direction) {
@@ -49,15 +57,30 @@ var snake = {
         this.snakeCoords.push([headX + 1, headY]);
         break;
     }
-
-    this.snakeCoords.shift();
+    // object variable for tail being removed
+    this.lastTail = this.snakeCoords.shift();
   },
-
-  reRenderSnake : function() {
+  // for translating the coordinates in the snake array
+  // to actual segments on the board
+  renderSnake : function() {
     //check coordinates of snakeCoords vs classes on board
     //remove any classes that don't match
     //
-    
+    var head = this.snakeHead()
+    var tail = this.lastTail
+    // we want to construct row(headX) and col(headY)
+    // remove tail at row(tail[0]) and col(tail[1])
+
+    this.findCoords(head).addClass("snake")
+    if (tail.length) this.findCoords(tail).removeClass("snake")
+  },
+  // this takes coords and converts them to jquery objects
+  findCoords : function(coords) {
+    var x = coords[0]
+    var y = coords[1]
+    var rowY = '.row' + y
+    var colX = '.col' + x
+    return $(rowY).filter(colX)
   },
 
   createFood : function(){
@@ -73,7 +96,7 @@ var snake = {
 
   },
 
-  //this is what the OnKeyUp event will call, 
+  //this is what the OnKeyUp event will call,
   //after parsing the keypress
   changeDirection : function(direction){
 
@@ -82,23 +105,34 @@ var snake = {
   //returns true if the snake has hit
   //itself or the end of the board
   collisionDetected : function(){
-    //add in true conditions
-    return false;
+    // set bounds of board from [0, 0] to [height,  width]
+    // if the snake head contains the coords of the bounds
+    // or itself, return true
+    var headX = this.snakeHead()[0];
+    var headY = this.snakeHead()[1];
+
+    if ( headX >= (this.width - 1) || headX < 0) {
+      return true;
+    } else if ( headY >= (this.height - 1) || headY < 0) {
+      return true;
+    } else {
+      return false;
+    }
+
   },
 
   //single turn, one frame
   //move the snake and check collisions
-  takeTurn : function(){
-    moveSnake();
-    reRenderSnake;
-  },
+  // takeTurn : function(){
+  // },
 
   gameLoop : function(){
-    while(true){
-      takeTurn();
-      if ( collisionDetected() ) break;
-      if ( touchingFood() ){ growSnake(); };
-    };
+    var loop = setInterval (function() {
+      snake.moveSnake();
+      snake.renderSnake();
+      if ( snake.collisionDetected() ) clearInterval(loop);
+      if ( snake.touchingFood() ){ snake.growSnake(); }
+    }, 1000)
   },
 
   buildBoard : function(){
@@ -120,15 +154,14 @@ var snake = {
   //sets up board
   //calls createSnake
   startGame : function(){
-    buildBoard();
-    createSnake();
-    createFood();
+    this.buildBoard();
+    this.createSnake();
+    this.createFood();
 
-    gameLoop();
+    this.gameLoop();
   }
 
 }; //end of snake game object
-
 
 
 
@@ -138,13 +171,11 @@ $(document).ready(function() {
 
   // just to test it worked
 
-  snake.buildBoard();
+  // snake.buildBoard();
+  snake.startGame();
 
 
-
-
-
-  //We need an on('keyup', function(){}) event 
+  //We need an on('keyup', function(){}) event
   // listener here
   //so you can control the snake
 
