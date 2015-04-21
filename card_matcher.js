9877fe631ad53806faffe8102e6e73982597f13c
Dan Berger <dsberger@gmail.com>
2015-04-21 09:49:35 -0700

    working card tiles

--- card_matcher.js
+++ card_matcher.js
@@ -1,4 +1,12 @@
 $( document ).ready(function() {
-  prompt("Please enter an even number to start your game!","4");
-
- });
+  var size = prompt("Please enter an even number to start your game!","4");
+  $container = $( '#container' )
+  $container.html('<h3>Voila! ' + size + '</h3>');
+  for (var i = 1; i <= size; i++) {
+    $container.append('<div class="row"></div>');
+    for (var z = 1; z <= size; z++) {
+      $row = $('#container .row').last();
+      $row.append('<div class="card"></div>');
+    }
+  }
+});
