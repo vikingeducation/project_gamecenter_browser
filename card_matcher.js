a9d5fb12752c49f350437cafc98cedbd0a129352
Dan Berger <dsberger@gmail.com>
2015-04-21 10:52:16 -0700

    added some card generation functions, page currently will not load.

--- card_matcher.js
+++ card_matcher.js
@@ -1,8 +1,7 @@
 var view = {
   init: function(){
-    var size = 1;
-    while(size % 2 === 1){
-      size = prompt("Please enter an even number to start your game!","4");
+    while(gridSize % 2 === 1){
+      gridSize = prompt("Please enter an even number to start your game!","4");
     }
 
     $container = $( '#container' );
@@ -11,7 +10,9 @@ var view = {
     }
   },
 
-  addRow: function (grid, columns){
+  gridSize: 1,
+
+  addRow: function(grid, columns){
     grid.append('<div class="row"></div>');
     for (var i = 1; i <= columns; i++) {
       $row = $('#container .row').last();
@@ -23,11 +24,38 @@ var view = {
 var controller = {
   init: function(){
     view.init();
+
+    var cardList = model.generateCardList(view.gridSize)
   }
 };
 
 var model = {
 
+  generateCardList: function(numOfCards){
+    model.shuffle(model.generateOrderedArray(numOfCards));
+  }
+
+  generateOrderedArray: function(numOfCards){
+    var orderedArray = [];
+    for( i = 1; i < numOfCards/2; i++ ) {
+      orderedArray.push(i);
+      orderedArray.push(i);
+    }
+    return orderedArray;
+  },
+
+  // Blatantly stolen from StackOverflow
+  shuffle: function(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex ;
+    while (0 !== currentIndex) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+    return array;
+  }
 };
 
 $( document ).ready(function() {
