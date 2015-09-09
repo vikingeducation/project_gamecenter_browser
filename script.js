c228d35c82d758f2284edd43cb86255c72d6a24b
Adam Kinson <adam.kinson@gmail.com>
2015-09-09 11:00:52 -0400

    set up base interval

--- script.js
+++ script.js
@@ -21,6 +21,39 @@ var model = {
       var startingUnit = model.findUnitByCoordinates(3,4);
       model.snake.units = [startingUnit];
       startingUnit.snake = true;
+    },
+
+    move: function() {
+      var newX = model.snake.units[0].x + model.snake.movementX();
+      var newY = model.snake.units[0].y + model.snake.movementY();
+      var newUnit = model.findUnitByCoordinates(newX, newY);
+      model.snake.units.unshift(newUnit);
+      model.snake.units.pop();
+      return newUnit;
+    },
+
+    movementX: function() {
+      if (model.snake.direction === 'right') {
+        return 1
+      }
+      else if (model.snake.direction === 'left') {
+        return -1
+      }
+      else {
+        return 0
+      };
+    },
+
+    movementY: function() {
+      if (model.snake.direction === 'down') {
+        return 1
+      }
+      else if (model.snake.direction === 'up') {
+        return -1
+      }
+      else {
+        return 0
+      };
     }
   },
 
@@ -76,6 +109,12 @@ var model = {
 
   getFoodIDs: function() {
     return model.food.unit[0].id;
+  },
+
+  nextFrame: function() {
+    var newUnit = model.snake.move();
+    // check snake collision
+    // check food collision
   }
 
 
@@ -128,6 +167,7 @@ var controller = {
     model.init(10);
     view.init(10);
     //start the loop
+    controller.play();
   },
 
   show: function() {
@@ -136,6 +176,19 @@ var controller = {
     var direction = model.getSnakeDirection();
     var foodID = model.getFoodIDs();
     view.renderFrame(snakeIDs, snakeHeadID, direction, foodID);
+  },
+
+  play: function() {
+    // every 2 seconds
+    setInterval(controller.gameloop, 2000);
+  },
+
+  gameloop: function() {
+    // update model
+    model.nextFrame();
+    // check for loss
+    // check for food gain
+    controller.show();
   }
 }
 
