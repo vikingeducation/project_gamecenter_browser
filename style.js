9ab1ac593f9ce0fce9a017cf95906ecbde8a7645
telwell <me@trevorelwell.com>
2015-10-12 18:04:54 -0400

    basic layout strategy

--- style.js
+++ style.js
@@ -1,9 +1,15 @@
+// Don't want to pullute the global namespace w. constants
+// so I'll add them to an object
+var const = {
+	grid_size: 6
+}
+
 var model = {
 
 }
 
 var view = {
-
+	
 }
 
 var controller = {
