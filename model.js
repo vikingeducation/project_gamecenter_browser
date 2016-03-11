8d7d6b7dbc83f4cfe5f01cecbf355230ccd06600
Thomas Lo <thomasjinlo@gmail.com>
2016-03-10 16:38:03 -0800

    Almost there

--- model.js
+++ model.js
@@ -2,6 +2,8 @@ var model = {
 
   max: 30,
   board: [],
+  snake: new Snake(),
+  food: new Food(),
 
   generateNewBoard: function() {
     this.board = new Array(this.max);
@@ -13,5 +15,39 @@ var model = {
   getBoard: function() {
     return this.board;
   }
+
+
+};
+
+function Snake() {
+
+  this.position = [[1,1]];
+  this.direction = 'r';
+
+  this.getPosition = function() {
+    return this.position 
+  };
+
+  this.setPosition = function(position) {
+    this.position.pop();
+    this.position.shift(position);
+  };
+
+  this.getDirection = function() {
+    return this.direction;
+  };
+
+  this.setDirection = function(direction) {
+    this.direction = direction;
+  };
+
+};
+
+function Food() {
+
+  var x = Math.floor(Math.random() * 10 + 10);
+  var y = Math.floor(Math.random() * 10 + 10);
+
+  this.position = [x, y];
 
 };
