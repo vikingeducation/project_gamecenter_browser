aaa61ba2698ee181cc824d1dabcb41e075fedeba
Joshua Masland-Sarani <j.masland@gmail.com>
2016-03-10 21:41:16 -0500

    trying a different way

--- snake_model.js
+++ snake_model.js
@@ -7,11 +7,6 @@ init: function(){
   var moves_array = [[-1,0], [1,0], [0,-1], [0,1]];
   var snake_direction = moves_array[Math.floor(Math.random()* moves_array.length)];
 
-  do{
-    var food_location = random_location();
-  }
-  while(food_location === snake_location);
-
   this.score = 0;
   this.board = new Board();
   this.snake = new Snake(snake_location, snake_direction);
@@ -20,7 +15,7 @@ init: function(){
 
 random_location: function(){
   return {
-    x: Math.floor(Math.random()*board_size), 
+    x: Math.floor(Math.random()*board_size),
     y: Math.floor(Math.random()*board_size)
   }
 },
@@ -47,26 +42,49 @@ random_location: function(){
 
 }
 
-function Food(starting_location){
-  this.location = starting_location;
+function Food(startingLocation){
+  this.location = startingLocation;
 }
 
-function Snake(starting_location, starting_direction){
-  this.location = starting_location;
-  this.direction = starting_direction;
-  this.next = null;
+function Snake(startingLocation, starting_direction){
+  this.segments = [ new Segment( startingLocation ) ];
+  this.direction = startingDirection;
+  this.growth = 0;
 
-  grow: function(){
-    if(this.next){
-      this.next.grow();
-    }
-    else{
-      this.next = new Snake();
-    }
+  this.move = function( direction ) {
+    var location = this.segments[0].location;
+    this.segments.pop();
+    this.segments.unshift( new Segment( this.findNextLocation( location, direction ) ) );
   }
 
+  this.findNextLocation = function(currentLocation, direction) {
+    switch( direction ) {
+    case "up":
+      currentLocation.y -= 1; break;
+    case "down":
+      currentLocation.y += 1; break;
+    case "left":
+      currentLocation.x -= 1; break;
+    case "right":
+      currentLocation.x += 1; break;
+    }
+  }
 }
 
-function Board(){
+function Segment(location) {
+  this.location = location;
+}
 
+function Board( boardSize ) {
+  this.grid = []
+  for ( var row = 0; row < boardSize; row++ ) {
+    this.grid[row] = [];
+    for ( var col = 0; col < boardSize; col++ ) {
+      this.grid[row][col] = new Cell();
+    }
+  }
+}
+
+function Cell() {
+  this.type = "none";
 }
