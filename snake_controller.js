0875c793ff5446faa97f7898e85fce2cfe144fbb
Sam O'Keefe <samok13@gmail.com>
2016-03-20 16:22:53 -0700

    working snake

--- snake_controller.js
+++ snake_controller.js
@@ -1,18 +1,31 @@
 var controller = {
 
   init: function(){
-    view.init();
     model.init();
-
-    model.addFood();//adds food obj
-    //adds snake obj
-
-    setInterval(function(){game_loop();}, 1000);
+    snake.init();
+    model.placeFood();
+    view.init();
+    this.game_over = false;
+    setInterval(this.gameLoop, 300);
   },
-  
-  game_loop: function(){
-  }
+  gameLoop: function(){
 
+    if (this.game_over){
+      return
+    }
+
+    if (!snake.grow()){
+      alert("You lose :(");
+        this.game_over = true;
+    }
+    
+    view.render();
+
+    if(model.wonGame()){
+      alert("You win!")
+      this.game_over = true;
+    }    
+  }
 }
 
 $(document).ready(function() {
