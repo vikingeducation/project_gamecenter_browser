21fe0b771037b34affc64aa07935b9c0e6876984
Steven Chang <prime_pork@hotmail.com>
2016-07-09 12:02:07 +1000

    Added mockups. Got a basic div layout happening. Header done

---
+++ snake.js
@@ -0,0 +1,23 @@
+'use_strict;'
+
+var controller = {
+  init: function(){
+
+  }
+};
+
+var model = {
+  init: function(){
+
+  }
+};
+
+var view = {
+  init: function(){
+
+  }
+};
+
+$(document).ready(function(){
+  controller.init();
+});
