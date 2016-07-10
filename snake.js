d1305b004909e2f1d86f9529b279e674a2329766
Steven Chang <prime_pork@hotmail.com>
2016-07-10 21:41:01 +1000

    game over sign flashes when you lose. fixed up bugs with intervals as well

--- snake.js
+++ snake.js
@@ -6,12 +6,9 @@ var controller = {
   		model.init();
   		view.init();
 
-	  	// I think we should set up the interval timer here...
-	  	// This really is the hub of everything cos every tick will have to set off a bunch of stuff.
-	  	controller.myInterval;
-  	},
+  		gameOverInterval = 0;
 
-  	myInterval: setInterval(function(){ 
+	  	myInterval = setInterval(function(){ 
   		// I need to know where the head is...
   		if (model.nextMoveIsPossible()) {
   			// Gotta move head to the next square
@@ -47,10 +44,18 @@ var controller = {
 			model.previousMove = model.nextMove;
   		} else {
   			// Stop this interval but start up another interval for the game over sign...
-  			clearInterval(controller.myInterval);
+  			clearInterval(myInterval);
   			controller.updateHighScores();
+  			gameOverInterval = setInterval(function(){
+			if ($("#game-over").length > 0) {
+				$("#game-over").remove();
+			} else {
+				$(".game-container").first().append("<div id='game-over'><h2>Game Over</h2></div>")
+			};
+		}, 1000);
   		};
-  	}, 600),
+  		}, 600)
+  	},
 
   	updateHighScores: function(){
   		model.highScores.push(model.lengthOfSnake - 1);
@@ -249,6 +254,32 @@ var model = {
   	} else {
   		return false;
   	};
+  },
+
+  directionOfNextBodyPart: function(rowNumber, columnNumber){
+  	var value = model.grid[rowNumber][columnNumber]
+	  	if (rowNumber > 0 && model.grid[rowNumber - 1][columnNumber] === value - 1) {
+	  		return "up"
+	  	} else if (rowNumber < 5 && model.grid[rowNumber + 1][columnNumber] === value - 1) {
+	  		return "down"
+	  	} else if (columnNumber > 0 && model.grid[rowNumber][columnNumber - 1] === value - 1) {
+	  		return "left"
+	  	} else {
+	  		return "right"
+	  	};
+  },
+
+  directionOfBodyPartBehindThisOne: function(rowNumber, columnNumber){
+  	var value = model.grid[rowNumber][columnNumber]
+	  	if (rowNumber > 0 && model.grid[rowNumber - 1][columnNumber] === value + 1) {
+	  		return "up"
+	  	} else if (rowNumber < 5 && model.grid[rowNumber + 1][columnNumber] === value + 1) {
+	  		return "down"
+	  	} else if (columnNumber > 0 && model.grid[rowNumber][columnNumber - 1] === value + 1) {
+	  		return "left"
+	  	} else {
+	  		return "right"
+	  	};
   }
 
 };
@@ -264,6 +295,17 @@ var view = {
   			model.nextMove = event.keyCode;
   		}
   	});
+
+  	$("#new-game").click(function(event){
+  		if (myInterval) {
+  			clearInterval(myInterval);
+  		};
+
+  		if (gameOverInterval) {
+  			clearInterval(gameOverInterval);
+  		};
+  		controller.init();
+  	});
   },
 
   // Adding and removing classes depending on model.grid
@@ -271,22 +313,83 @@ var view = {
   	for (var rowNumber = 0; rowNumber < model.grid.length; rowNumber++) {
   		for (var columnNumber = 0; columnNumber < model.grid[rowNumber].length; columnNumber++) {
   			switch(model.grid[rowNumber][columnNumber]) {
-  				case 0: 
-  					$(".game-square").eq([rowNumber * 6 + columnNumber]).addClass("head").removeClass("mouse body tail");
+  				case 0:
+  					var $head = $(".game-square").eq([rowNumber * 6 + columnNumber]);
+  					$head.attr("class","game-square").addClass("head");
+  					view.styleSnake($head, "head");
   					break;
   				case "m": 
-  					$(".game-square").eq([rowNumber * 6 + columnNumber]).addClass("mouse").removeClass("head body tail");
+  					$(".game-square").eq([rowNumber * 6 + columnNumber]).attr("class","game-square").addClass("mouse");
   					break;
   				case undefined:
-  					$(".game-square").eq([rowNumber * 6 + columnNumber]).removeClass("mouse head body tail");
+  					$(".game-square").eq([rowNumber * 6 + columnNumber]).attr("class","game-square");
   					break;
-  				default: 
-  					$(".game-square").eq([rowNumber * 6 + columnNumber]).addClass("body").removeClass("mouse head tail");
+  				default:
+  					var $body = $(".game-square").eq([rowNumber * 6 + columnNumber]);
+  					$body.attr("class","game-square").addClass("body");
+  					view.styleSnake($body, "body", rowNumber, columnNumber, model.grid[rowNumber][columnNumber]);
   			};
 
   			if(model.lengthOfSnake >= 2 && model.grid[rowNumber][columnNumber] === model.lengthOfSnake - 1){
-  				$(".game-square").eq([rowNumber * 6 + columnNumber]).addClass("tail").removeClass("mouse head body");
+  				var $tail = $(".game-square").eq([rowNumber * 6 + columnNumber]);
+  				$tail.attr("class","game-square").addClass("tail");
+  				view.styleSnake($tail, "tail", rowNumber, columnNumber, model.grid[rowNumber][columnNumber]);
   			};
+  		};
+  	};
+  },
+
+  removeBorderForBodyPartAhead: function($square, rowNumber, columnNumber){
+  	if (model.directionOfNextBodyPart(rowNumber, columnNumber) === "up") {
+		$square.addClass("body-up");
+	} else if (model.directionOfNextBodyPart(rowNumber, columnNumber) === "down") {
+		$square.addClass("body-down");
+	} else if (model.directionOfNextBodyPart(rowNumber, columnNumber) === "left") {
+		$square.addClass("body-left");
+	} else {
+		$square.addClass("body-right");
+	};
+  },
+
+  removeBorderForBodyPartBehind: function($square, rowNumber, columnNumber){
+  	if (model.directionOfBodyPartBehindThisOne(rowNumber, columnNumber) === "up") {
+		$square.addClass("body-up");
+	} else if (model.directionOfBodyPartBehindThisOne(rowNumber, columnNumber) === "down") {
+		$square.addClass("body-down");
+	} else if (model.directionOfBodyPartBehindThisOne(rowNumber, columnNumber) === "left") {
+		$square.addClass("body-left");
+	} else {
+		$square.addClass("body-right");
+	};
+  },
+
+  styleSnake: function($square, type, rowNumber, columnNumber, value) {
+  	if (type === "head") {
+  		// Left
+  		if (model.nextMove === 37) {
+  			$square.addClass("head-left");
+  		// Top
+  		} else if (model.nextMove === 38) {
+  			$square.addClass("head-up");
+  		// Right
+  		} else if (model.nextMove === 39) {
+  			$square.addClass("head-right");
+  		} else {
+  		// Bottom
+  			$square.addClass("head-down");
+  		};
+  	} else if (type === "body") {
+  		view.removeBorderForBodyPartAhead($square, rowNumber, columnNumber);
+  		view.removeBorderForBodyPartBehind($square, rowNumber, columnNumber);
+  	} else {
+  		if (model.directionOfNextBodyPart(rowNumber, columnNumber) === "up") {
+  			$square.addClass("tail-up");
+  		} else if (model.directionOfNextBodyPart(rowNumber, columnNumber) === "down") {
+  			$square.addClass("tail-down");
+  		} else if (model.directionOfNextBodyPart(rowNumber, columnNumber) === "left") {
+  			$square.addClass("tail-left");
+  		} else {
+  			$square.addClass("tail-right");
   		};
   	};
   }
