ceb7c55d76ed1f9b81f554052177e8d4c7e5d9db
Graham Turner <turnertgraham@gmail.com>
2016-09-09 19:27:10 -0400

    animate that snake

--- script.js
+++ script.js
@@ -1,32 +1,61 @@
 $(document).ready(function() {
-  var grid = $('.grid');
-  for (var i = 0; i < 64; i++) {
-    console.log(grid)
-    grid.append('<div class="gridpiece"></div>');
-  }
+  Controller.init();
 });
 
 var View = {
 
-  render: function(array) {
+  init: function() {
+    this.buildGrid();
+    this.checkForTurn();
+  },
+
+  checkForTurn: function() {
+    $('body').keydown(function(e) {
+      Controller.changeDirection(e.keyCode);
+
+    });
+  },
+
+  buildGrid: function() {
     var grid = $('.grid');
-    grid.children.removeClass('highlight');
+    for (var i = 0; i < 64; i++) {
+      grid.append('<div class="gridpiece"></div>');
+    }
+  },
+
+  render: function(array, food) {
+    var grid = $('.grid');
+    grid.children().removeClass('highlight').removeClass('food');
     for (var i = 0; i < array.length; i++) {
       $( ".grid div:nth-child(" + array[i] + ")" ).addClass('highlight');
     }
+    $( ".grid div:nth-child(" + food + ")" ).addClass('food');
   }
 
 };
 
 var Controller = {
 
-  init: function() {
-
+  KEYCODES: {
+    '38': 'n',
+    '37': 'w',
+    '40': 's',
+    '39': 'e'
   },
 
-  renderPage: function() {
-    View.render(Model.snakeArray);
-  }
+  init: function() {
+    View.init();
+    setInterval(function() {
+      Model.init();
+      View.render(Model.snakeArray, Model.food);
+      Model.checkFood();
+      View.render(Model.snakeArray, Model.food);
+    }, 500);
+  },
+
+  changeDirection: function(code) {
+    Model.direction = this.KEYCODES[String(code)];
+  },
 
 };
 
@@ -41,22 +70,42 @@ var Model = {
   },
 
   init: function() {
-    this.startSnake();
+    this.tick();
   },
 
-  snakeArray: [1,2,3],
+  snakeArray: [3,2,1],
 
-  food: 0,
+  food: 50,
+
+  randomNum: function() {
+    do
+      var num = Math.floor(Math.random() * 64) + 1;
+    while (this.snakeArray.includes(num))
+    return num;
+  },
 
   direction: "e",
 
-  startSnake: function() {
-    setInterval(function() {
-      this.snakeArray.pop();
-      var head = updateSnake(this.snakeArray[0]);
-      this.snakeArray.unshift(head);
-      Controller.renderPage();
-    }, 1000)
+  tick: function() {
+    this.snakeArray.pop();
+    var head = this.updateSnake(this.snakeArray[0]);
+    this.snakeArray.unshift(head);
+  },
+
+  getDiff: function() {
+    var array = this.snakeArray;
+    console.log(array[array.length - 2], array[array.length - 1]);
+    var diff = array[array.length - 2] - array[array.length - 1];
+    return array[array.length - 1] - diff;
+  },
+
+  checkFood: function() {
+    if (this.snakeArray[0] === this.food) {
+      this.food = this.randomNum();
+      var tail = this.getDiff();
+      console.log(tail);
+      this.snakeArray.push(tail);
+    }
   },
 
   updateSnake: function(head) {
