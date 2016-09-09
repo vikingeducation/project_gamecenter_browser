4c86852ae2e99d9437703f4f1005b69da6b0ef31
“Matthew <matthew.hinea@gmail.com>
2016-09-09 15:59:37 -0700

    finish basic model

--- script.js
+++ script.js
@@ -4,6 +4,8 @@
 function Snake() {
   this.length = 1;
   this.head = new Section();
+  this.headX = 3;
+  this.headY = 3;
 }
 
 function Section(nextSection) {
@@ -22,6 +24,7 @@ view = {
 gridModel = {
 
   grid: [],
+
   init: function(size) {
     for (var i = 0; i < size; i++) {
       this.grid.push([]);
@@ -29,31 +32,95 @@ gridModel = {
         this.grid[i].push(null);
       }
     }
+    gridModel.placeSnake();
   },
+
   placeFood: function() {
     do {
-    var x = Math.random(this.grid.length)
-    var y =Math.random(this.grid.length)
-    }
-    while {
+    var x = Math.random(this.grid.length);
+    var y =Math.random(this.grid.length);
+    } while {
       // x or y is occupied by snake.
     }
   },
-  moveSnake: function() {
 
+  placeSnake: function() {
+    var x = this.snake.headX;
+    var y = this.snake.headY;
+    this.grid[x][y] = this.snake.head;
+  },
+
+  initSnake: function() {
+    this.snake = new Snake();
+    return this.snake;
+  },
+
+  moveSnake: function() {
+    var x = this.snake.headX;
+    var y = this.snake.headY;
+    switch(controller.currentDirection) {
+      case "left":
+        x -= 1;
+        break;
+
+      case "right":
+        x += 1;
+        break;
+
+      case "up":
+        y += 1;
+        break;
+
+      case "down":
+        y -= 1;
+        break;
+
+      default: return;
+    };
+    this.snake.headX = x;
+    this.snake.headY = y;
+    placeSnake();
   }
 
 }
 
 controller = {
 
+  currentDirection: "none", 
+
   init: function() {
+    this.setEventListeners();
     interval = setInterval(gridModel.moveSnake(), 200);
     view.init();
   },
 
   gameOver: function() {
     clearInterval(interval);
+  },
+
+  setEventListeners: function() {
+    $(document).keydown(function(e) {
+      switch(e.which) {
+        case 37: // left
+        this.currentDirection = "left";
+        break;
+
+        case 38: // up
+        this.currentDirection = "up";
+        break;
+
+        case 39: // right
+        this.currentDirection = "right";
+        break;
+
+        case 40: // down
+        this.currentDirection = "down";
+        break;
+
+        default: return; // exit this handler for other keys
+      }
+      e.preventDefault(); // prevent the default action (scroll / move caret)
+    });
   }
 
 }
