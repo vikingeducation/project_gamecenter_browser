2de8c0a1a5ea3e4021281e52dfc8a8668e6640fb
Leo Ahnn <leo.ahnn@gmail.com>
2016-09-09 19:38:31 -0400

    working on snake

--- script.js
+++ script.js
@@ -4,8 +4,6 @@
 function Snake() {
   this.length = 1;
   this.head = new Section();
-  this.headX = 3;
-  this.headY = 3;
 }
 
 function Section(nextSection) {
@@ -16,15 +14,34 @@ function Section(nextSection) {
 
 
 view = {
-  init: function() {
-    //
+
+  render: function() {
+    var gridSize = controller.getGridSize();
+    for (var i = 0; i < gridSize; i++) {
+      var $row = $('<div></div>');
+      $row.addClass('row');
+      $row.prependTo('#game-grid');
+      for (var j = 0; j < gridSize; j++) {
+        var $cell = $('<div></div>');
+        // if grid is empty
+        var cellContent = controller.getGridCell(j,i)
+        if (cellContent === "food") {
+          $cell.addClass("food");
+        } else if (cellContent === "snake") {
+          $cell.addClass("snake");
+        }
+        $cell.addClass('cell');
+        $cell.attr('id', String(j) + "-" + String(i));
+        $cell.appendTo($row);
+      }
+    }
   }
+
 }
 
 gridModel = {
 
   grid: [],
-
   init: function(size) {
     for (var i = 0; i < size; i++) {
       this.grid.push([]);
@@ -32,96 +49,50 @@ gridModel = {
         this.grid[i].push(null);
       }
     }
-    gridModel.placeSnake();
   },
-
   placeFood: function() {
     do {
-    var x = Math.random(this.grid.length);
-    var y =Math.random(this.grid.length);
-    } while {
+      var x = Math.random(this.grid.length)
+      var y = Math.random(this.grid.length)
+    }
+    while (false) {
       // x or y is occupied by snake.
     }
+    grid[x][y] = "food";
   },
-
-  placeSnake: function() {
-    var x = this.snake.headX;
-    var y = this.snake.headY;
-    this.grid[x][y] = this.snake.head;
-  },
-
-  initSnake: function() {
-    this.snake = new Snake();
-    return this.snake;
-  },
-
   moveSnake: function() {
-    var x = this.snake.headX;
-    var y = this.snake.headY;
-    switch(controller.currentDirection) {
-      case "left":
-        x -= 1;
-        break;
 
-      case "right":
-        x += 1;
-        break;
-
-      case "up":
-        y += 1;
-        break;
-
-      case "down":
-        y -= 1;
-        break;
-
-      default: return;
-    };
-    this.snake.headX = x;
-    this.snake.headY = y;
-    placeSnake();
   }
 
 }
 
 controller = {
 
-  currentDirection: "none", 
-
   init: function() {
-    this.setEventListeners();
+    gridModel.init(10);
     interval = setInterval(gridModel.moveSnake(), 200);
-    view.init();
+    view.render();
   },
 
   gameOver: function() {
     clearInterval(interval);
   },
 
-  setEventListeners: function() {
-    $(document).keydown(function(e) {
-      switch(e.which) {
-        case 37: // left
-        this.currentDirection = "left";
-        break;
+  getGridSize: function() {
+    return gridModel.grid.length;
+  },
 
-        case 38: // up
-        this.currentDirection = "up";
-        break;
-
-        case 39: // right
-        this.currentDirection = "right";
-        break;
-
-        case 40: // down
-        this.currentDirection = "down";
-        break;
-
-        default: return; // exit this handler for other keys
-      }
-      e.preventDefault(); // prevent the default action (scroll / move caret)
-    });
+  getGridCell: function(x, y) {
+    var cell = gridModel.grid[x][y];
+    if (cell === "food") {
+      return "food";
+    } else if (cell) {
+      return "snake";
+    } else {
+      return null;
+    }
   }
+
 
 }
 
@@ -129,6 +100,5 @@ controller = {
 
 
 $(document).ready(function() {
-
-
+  controller.init();
 })
