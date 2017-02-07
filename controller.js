b407df0f8a54f4a8778a2fe4dd5c250967bfa8da
Kenneth Kottenstette <kennykotten@gmail.com>
2017-02-06 20:45:30 -0800

    workstation switch

--- controller.js
+++ controller.js
@@ -1,5 +1,14 @@
 'use strict';
 
 var controller = {
+  init: function(){
+    
+  },
+
+
 
 };
+
+$(document).ready(function(){
+  controller.init();
+});
--- model.js
+++ model.js
@@ -1,5 +1,24 @@
 'use strict';
 
 var model = {
+  // in mili-seconds
+  gameSpeed: 200,
+  rows: 25,
+  columns: 25,
+  points: 0,
+
+
+  board: {
+
+  },
+
+  snake: {
+
+  },
+
+  food: {
+
+  }
+
 
 };
